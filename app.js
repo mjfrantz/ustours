3a65e10da72972dff7f9ce6dee6ce6e39e1d20fb
Michael Frantz <mjfrantz26@gmail.com>
2020-05-12 18:00:44 +0900

    Adding controllers and refactoring

--- app.js
+++ app.js
@@ -1,93 +1,27 @@
-const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+//Middleware
+//This middleware applies to each and every request if the route handler comes before the route handler.
+app.use(morgan('dev')); // logger the time it took to send back the response 
 app.use(express.json());
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours
-    }
-  });
-};
-
-const getTour = (req, res) => {
-  console.log(req.params);
-
-  const id = req.params.id * 1;
-
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id'
-    });
-  };
-
-  const tour = tours.find(el => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-      status: 'success',
-      data: { tour: newTour }
-    });
-  });
-};
-
-const updateTour = (req, res) => {
-
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id'
-    });
-  };
-
-  res.status(200).json({
-    status: 'success',
-    data: { tour: '< Updated tour here...>' }
-  });
-}
-
-const deleteTour = (req, res) => {
-
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id'
-    });
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
-};
-
-//Refactored code after the endpoint, we call the function 
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
-//start up a server with a callback function
-const port = 3000;
-app.listen(port, () => {
-  console.log(`app running on port ${port}`);
+app.use((req, res, next) => {
+  console.log('Hello from Middleware');
+  next();
 });
 
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+})
+
+//Routes (Middleware)
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
