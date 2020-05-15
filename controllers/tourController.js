9952c94607c3807a02ad006240136bd937f720ba
Michael Frantz <mjfrantz26@gmail.com>
2020-05-15 14:24:57 +0900

    Adding the Filtering query method

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,11 +1,22 @@
 const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
-  //The find method will return everything in the Tour Collection
-  //This will return a promise we will await
   try {
-    const tours = await Tour.find();
+    //Build Query
+    const queryObject = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObject[el]);
 
+    console.log(req.query, queryObject);
+
+    //will return a query object
+    //Come back with the documents we have queried
+    const query = await Tour.find(queryObject);
+
+    //Execute query
+    const tours = await query;
+
+    //Send response
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -22,7 +33,6 @@ exports.getAllTours = async (req, res) => {
 };
 
 exports.getTour = async (req, res) => {
-  //findById is the key and the value is the id.
   try {
     const tour = await Tour.findById(req.params.id);
     res.status(200).json({
@@ -41,9 +51,6 @@ exports.getTour = async (req, res) => {
 
 exports.createTour = async (req, res) => {
   try {
-    //we call the create method on the Tour model directly
-    //this create method require the request body which return a promise now using async await
-    //Once the promise is resolved we save the results to the variable newTour
     const newTour = await Tour.create(req.body);
 
     res.status(201).json({
@@ -53,7 +60,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data set',
+      message: err,
     });
   }
 };
@@ -61,8 +68,8 @@ exports.createTour = async (req, res) => {
 exports.updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-      new: true, //new updated document will be the one returned
-      runValidators: true, // makes sure it runs against the schema and the validators are correct.
+      new: true,
+      runValidators: true,
     });
     res.status(200).json({
       status: 'success',
